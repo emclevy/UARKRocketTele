72ebd2b83f32d208982080859881bbb4dd23f7a2
Rodrigo <95721937+rjmouron01@users.noreply.github.com>
2023-01-29 17:51:59 -0600

    peaks working

--- RocketTele.js
+++ RocketTele.js
@@ -1,12 +1,50 @@
 
 
 let count = setInterval(display, 300);
-let end = 15;
+//let end = 15;
 function display(){
-        var answer = document.getElementById('Accel');
-        answer.value = ++end;
-        if(end === 200) {
-            clearInterval(count)
+        //live data variables
+        var acceleration = document.getElementById('Accel');
+        var speed = document.getElementById('Vel');
+        //var altitude = document.getElementById('')
+        //peak variables
+        var peakAcceleration = document.getElementById('PAccel');
+        var peakSpeed = document.getElementById('PSpeed');
+        // var peakAltitude = document.getElementById('PAlti');
+        
+        //get value for speed
+        var spee = speed.value;
+        spee++
+        speed.value = spee;
+        //get value for acc
+        var acc = acceleration.value;
+        acc++
+        acceleration.value = acc;
+        //get value for alt
+        // var alt = altitude.value;
+        // alt++
+        // altitude.value = alt;
+        // peakAltitude = alt;
+
+        //gets peak speed
+        if(peakSpeed.value <= spee) {
+            peakSpeed.value = spee;
+        }
+        //gets peak acceleration value
+        if(peakAcceleration.value <= acc) {
+            peakAcceleration.value = acc;
+        }
+        //gets peak alt
+        
+
+        //resets testing back to 0 
+        if(speed.value == 50) {
+            speed.value = 0;
+            clearInterval(count);
+        }
+        if(acceleration.value == 50) {
+            acceleration.value = 0;
+            clearInterval(count);
         }
     }
 
