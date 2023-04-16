a8f873e7f218420f4aae1cf1e54f6d6050e8fb33
Kormah Dorko <74940467+Howstressful@users.noreply.github.com>
2023-04-16 15:42:57 -0500

    updated the display of data

--- RocketTele.js
+++ RocketTele.js
@@ -1,5 +1,4 @@
 
-
 let count = setInterval(display, 300);
 //let end = 15;
 function display(){
@@ -10,21 +9,62 @@ function display(){
         //peak variables
         var peakAcceleration = document.getElementById('PAccel');
         var peakSpeed = document.getElementById('PSpeed');
-        // var peakAltitude = document.getElementById('PAlti');
+        var peakAltitude = document.getElementById('PAlti');
+        var ejectAlt = document.getElementById('Eject');
         
+        var boost1 = document.getElementById('Boost1');
+        var boost2 = document.getElementById('Boost2');
+        var boost3 = document.getElementById('Boost3');
+        var volt = document.getElementById('Volt');
+
+    
         //get value for speed
-        var spee = speed.value;
+        var spee = parseFloat(speed.value);
         spee++
-        speed.value = spee;
+        speed.value = spee.toFixed(2) + " m/s"; // Replace "units" with the actual units (e.g., "m/s")
+
+        
         //get value for acc
-        var acc = acceleration.value;
+        var acc = parseFloat(acceleration.value);
         acc++
-        acceleration.value = acc;
+        acceleration.value = acc.toFixed(2) + " m/s²";
+
+        
         //get value for alt
-        // var alt = altitude.value;
-        // alt++
+        var alt = 0;
+        alt++
         // altitude.value = alt;
         // peakAltitude = alt;
+        
+
+        //boost
+        if(boost1.value < 16){
+            boost1.value++;
+        } else if(boost2.value < 32 && boost1.value === 16){
+            boost2.value++;
+        } else if(boost3.value < 48 && boost2.value === 32){
+            boost3.value++;
+        }
+
+
+        //assigns a voltage value
+        //assigns a voltage value
+        var vv = volt.value;
+        if (spee < 25) {
+          vv = 1.3;
+        } else if (spee >= 25 && spee < 40) {
+          vv += 0.005;
+          if (vv > 1.8) {
+            vv = 1.8;
+          }
+        } else if (spee >= 40) {
+          vv -= 0.005;
+          if (vv < 1.3) {
+            vv = 1.3;
+          }
+        }
+        volt.value = vv;
+        
 
         //gets peak speed
         if(peakSpeed.value <= spee) {
@@ -35,11 +75,33 @@ function display(){
             peakAcceleration.value = acc;
         }
         //gets peak alt
+        if(peakAltitude.value <= alt){
+            peakAltitude.value = alt;
+        }
+
+        // boosters
+  var boost1MaxSpeed = 16;
+  var boost2MaxSpeed = 32;
+  var boost3MaxSpeed = 48;
+  if (boost1.value < boost1MaxSpeed) {
+    boost1.value++;
+  } else if (boost2.value < boost2MaxSpeed) {
+    boost2.value++;
+  } else if (boost3.value < boost3MaxSpeed) {
+    boost3.value++;
+  }
+
+        //voltage
+        if(speed.value >= 25){
+            volt.value = 1.8 ;
+        }
+
         
 
         //resets testing back to 0 
         if(speed.value == 50) {
             speed.value = 0;
+            volt.value = 0; /* added this to show a change in voltgage */
             clearInterval(count);
         }
         if(acceleration.value == 50) {
@@ -47,7 +109,8 @@ function display(){
             clearInterval(count);
         }
 
-
+       
+         
     }
 
 
