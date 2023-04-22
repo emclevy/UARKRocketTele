84b28d709ba823e937566e0be313c4668cd07789
TroyJones0 <89871014+TroyJones0@users.noreply.github.com>
2023-04-21 22:24:46 -0500

    Update main.c
    completed writing to file and adding time functionality to match sim

--- SoC/TestData/main.c
+++ SoC/TestData/main.c
@@ -4,18 +4,28 @@
 #include <time.h>
 #include <unistd.h>
 
-#define fileName "dataFile.csv"
+#define fileName "RawData.csv"
+#define outputFile "realtime.csv"
 #define timeColumn 0
 #define AltitudeColumn 1
 #define VelocityColumn 2
 #define AccelerationColumn 3
+#define VerticalOri 4
+#define LateralOri 5
 
-
+float maxAlt;
+float maxVel;
+float maxAcc;
+float curTime;
+float elapsed_time = 0;
+float prevTime = 0;
 int parseFlag = 1;
 float dataSet[23][450];
 char TelemNames[23];
 volatile float package[5]; //T, Alt, Veloc, AccelG
 int row = 0;
+
+void delay(int milli_seconds);
 
 float getValue(int row_p, int dataCol){
 
@@ -32,7 +42,7 @@ float getValue(int row_p, int dataCol){
         while (fgets(line, 1024, dataFile) != NULL){
             token = strtok(line, ",");
             cur_col = 0;
-            while (token != NULL && cur_col < 23) {
+            while (token != NULL && cur_col < 6) {
 
                 data[cur_col] = atof(token);
                 cur_col++;
@@ -49,24 +59,55 @@ float getValue(int row_p, int dataCol){
     }
 }
 
-void getTime(int row_p){
+void getTime(int row_p, FILE *fp){
     //todo: read current value from system clock on msp430
-    printf("Time: %f\n", getValue(row_p, timeColumn));
+    curTime = getValue(row_p, timeColumn) * 1000;
+    delay((curTime - prevTime) * 1000);
+    printf("Time: %.2f\n", getValue(row_p, timeColumn));
+    fprintf(fp, "%.2f,", getValue(row_p, timeColumn));
+    prevTime = curTime;
 }
 
-void getAcceleration(int row_p){
+void getAcceleration(int row_p, FILE *fp){
     //todo: read current value from accelerometer on msp430
-    printf("Acceleration: %f\n", getValue(row_p, AccelerationColumn));
+    float accel = getValue(row_p, AccelerationColumn);
+    printf("Acceleration: %.2f\n", accel);
+    fprintf(fp, "%.2f,", getValue(row_p, AccelerationColumn));
+    if (accel > maxAcc){
+        maxAcc = accel;
+    }
 }
 
-void getVelocity(int row_p){
+void getVelocity(int row_p, FILE *fp){
     //todo: read current velocity from msp430
-    printf("Velocity: %f\n", getValue(row_p, VelocityColumn));
+    float velocity = getValue(row_p, VelocityColumn);
+    fprintf(fp, "%.2f,", getValue(row_p, VelocityColumn));
+    printf("Velocity: %.2f\n", velocity);
+    if (velocity > maxVel){
+        maxVel = velocity;
+    }
 }
 
-void getAltitude(int row_p){
+void getAltitude(int row_p, FILE *fp){
     //todo: read current altitude from msp430
-    printf("Acceleration: %f\n", getValue(row_p, AltitudeColumn));
+    float altitude = getValue(row_p, AltitudeColumn);
+    fprintf(fp, "%.2f,", getValue(row_p, AltitudeColumn));
+    printf("Altitude: %.2f\n", altitude);
+    if (altitude > maxAlt){
+        maxAlt = altitude;
+    }
+}
+
+void getVertOri(int row_p, FILE *fp){
+    //todo: read current value from system clock on msp430
+    printf("Vertical Orientation: %.2f\n", getValue(row_p, VerticalOri));
+    fprintf(fp, "%.2f,", getValue(row_p, VerticalOri));
+}
+
+void getLatOri(int row_p, FILE *fp){
+    //todo: read current value from system clock on msp430
+    printf("Lateral Orientation: %.2f\n", getValue(row_p, LateralOri));
+    fprintf(fp, "%.2f\n", getValue(row_p, LateralOri));
 }
 
 void getGPS(){
@@ -77,25 +118,49 @@ void getEMatchStates() {
 
 }
 
-void scheduler(int row_p) {
+void scheduler(int row_p, FILE *fp) {
 
-    getTime(row_p);
-    getAltitude(row_p);
-    getVelocity(row_p);
-    getAcceleration(row_p);
+    getTime(row_p, fp);
+    getAltitude(row_p, fp);
+    getVelocity(row_p, fp);
+    getAcceleration(row_p, fp);
+    getVertOri(row_p, fp);
+    getLatOri(row_p, fp);
+    fflush(fp); //write data to disk immediately
 
 }
 
+
+
+void delay(int milli_seconds) {
+    // Converting time into milli_seconds
+    //int milli_seconds = 1000 * number_of_milliseconds;
+
+    // Storing start time
+    clock_t start_time = clock();
+
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds);
+}
+
+
 int main() {
+    FILE *output = fopen(outputFile, "w");
+    if(output == NULL){
+        printf("error");
+    }
+    fprintf(output, "Time, Altitude, Velocity, Acceleration, vertical orientation, lateral orientation\n");
     //clock_t start_time = clock();
     printf("Hello, World!\n");
-    for (int i = 0; i <=10; i++){
-        printf("data at %i\n", i);
-        scheduler(i);
+    for (int i = 0; i <=645; i++){
+        printf("\n \ndata at %i\n", i);
+        scheduler(i, output);
+        //delay(100000);
     }
-    //scheduler();
+    printf("Max alt: %f \n", maxAlt);
+    printf("Max vel: %f \n", maxVel);
+    printf("Max acc: %f \n", maxAcc);
 
-
-        //sleep(5);
+    //sleep(1000);
     return 0;
 }
