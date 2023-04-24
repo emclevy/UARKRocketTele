72c68f2fa482e2d1a3faac5322d17b3257fccc2e
TroyJones0 <89871014+TroyJones0@users.noreply.github.com>
2023-04-24 15:23:47 -0500

    Update i2c.c

--- SoC/Sensors/i2c.c
+++ SoC/Sensors/i2c.c
@@ -14,74 +14,74 @@ void i2c_init()
   P4SEL |= BIT1 | BIT2; // Assign I2C pins to USCI_B0 IMU/ALT_SCL and SDA is on pins 4.1/4.2
 
   // Configure USCI_B0 for I2C mode
-  UCB0CTL1 |= UCSWRST; // Enable SW reset
-  UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC; // I2C Master, synchronous mode
-  UCB0CTL1 = UCSSEL_2 + UCSWRST; // Use SMCLK, keep SW reset
-  UCB0BR0 = 80; // fSCL = SMCLK/80 = ~200kHz
-  UCB0BR1 = 0;
-  UCB0CTL1 &= ~UCSWRST; // Clear SW reset, resume operation
-  UCB0IE |= UCTXIE + UCRXIE; // Enable TX and RX interrupts
+  UCB1CTL1 |= UCSWRST; // Enable SW reset
+  UCB1CTL0 = UCMST + UCMODE_3 + UCSYNC; // I2C Master, synchronous mode
+  UCB1CTL1 = UCSSEL_2 + UCSWRST; // Use SMCLK, keep SW reset
+  UCB1BR0 = 120; // fSCL = SMCLK/80 = ~200kHz
+  UCB1BR1 = 0;
+  UCB1CTL1 &= ~UCSWRST; // Clear SW reset, resume operation
+  //UCB1IE |= UCTXIE + UCRXIE; // Enable TX and RX interrupts
 }
 
 void i2c_start()
 {
   // Generate a START condition
-  UCB0CTL1 |= UCTR + UCTXSTT; // I2C TX, START condition
+  UCB1CTL1 |= UCTR + UCTXSTT; // I2C TX, START condition
 }
 
 void i2c_stop()
 {
   // Generate a STOP condition
-  UCB0CTL1 |= UCTXSTP; // I2C STOP condition
-  while (UCB0CTL1 & UCTXSTP); // Wait for STOP condition
+  UCB1CTL1 |= UCTXSTP; // I2C STOP condition
+  while (UCB1CTL1 & UCTXSTP); // Wait for STOP condition
 }
 
 void i2c_write(uint8_t addr, uint8_t reg, uint8_t *data, uint8_t len)
 {
-    UCB0I2CSA = addr;
+    UCB1I2CSA = addr;
 
-    UCB0CTL1 |= UCTR | UCTXSTT; // Transmit mode and send start condition
+    UCB1CTL1 |= UCTR | UCTXSTT; // Transmit mode and send start condition
 
-    while (!(UCB0IFG & UCTXIFG));
-    UCB0TXBUF = reg;
+    //while (!(UCB1IFG & UCTXIFG));
+    UCB1TXBUF = reg;
 
     uint8_t i = 0;
     for (i; i < len; i++)
     {
-        while (!(UCB0IFG & UCTXIFG));
-        UCB0TXBUF = data[i];
+        //while (!(UCB1IFG & UCTXIFG));
+        UCB1TXBUF = data[i];
     }
 
-    while (!(UCB0IFG & UCTXIFG));
-    UCB0CTL1 |= UCTXSTP; // Send stop condition
+    //while (!(UCB1IFG & UCTXIFG));
+    UCB1CTL1 |= UCTXSTP; // Send stop condition
 
-    while (UCB0CTL1 & UCTXSTP);
+    while (UCB1CTL1 & UCTXSTP);
 }
 
 void i2c_read(uint8_t addr, uint8_t reg, uint8_t *data, uint8_t len)
 {
-    UCB0I2CSA = addr;
+    UCB1I2CSA = addr;
 
-    UCB0CTL1 |= UCTR | UCTXSTT; // Transmit mode and send start condition
+    UCB1CTL1 |= UCTR | UCTXSTT; // Transmit mode and send start condition
 
-    while (!(UCB0IFG & UCTXIFG));
-    UCB0TXBUF = reg;
+    //while (!(UCB1IFG & UCTXIFG));
+    UCB1TXBUF = reg;
 
-    while (!(UCB0IFG & UCTXIFG));
-    UCB0CTL1 &= ~UCTR; // Receive mode
-    UCB0CTL1 |= UCTXSTT; // Send repeated start condition
-   
+    //while (!(UCB1IFG & UCTXIFG));
+    UCB1CTL1 &= ~UCTR; // Receive mode
+    UCB1CTL1 |= UCTXSTT; // Send repeated start condition
+
 
     uint8_t i = 0;
     for (i; i < len - 1; i++)
     {
-        while (!(UCB0IFG & UCRXIFG));
-        data[i] = UCB0RXBUF;
+        while (!(UCB1IFG & UCRXIFG));
+        data[i] = UCB1RXBUF;
     }
 
-    UCB0CTL1 |= UCTXSTP; // Send stop condition
-    while (!(UCB0IFG & UCRXIFG));
-    data[len - 1] = UCB0RXBUF;
+    UCB1CTL1 |= UCTXSTP; // Send stop condition
+    //while (!(UCB1IFG & UCRXIFG));
+    data[len - 1] = UCB1RXBUF;
 
-    while (UCB0CTL1 & UCTXSTP);
+    //while (UCB1CTL1 & UCTXSTP);
 }
